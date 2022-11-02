056d262c0e93fbbea5d630b8d8e427fe8decc4a8
gaon-park <do.judo1224@gmail.com>
2022-11-02 20:58:51 +0900

    숫자 짝꿍

---
+++ src/lev1/Solution131128.java
@@ -0,0 +1,28 @@
+package lev1;
+
+import java.util.*;
+
+public class Solution131128 {
+    public String solution(String x, String y) {
+        List<String> listX = new ArrayList<>(Arrays.asList(x.split("")));
+        List<String> listY = new ArrayList<>(Arrays.asList(y.split("")));
+        String[] arr = {"9", "8", "7", "6", "5", "4", "3", "2", "1", "0"};
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            if (listX.contains(s) && listY.contains(s)) {
+                int count = Math.min(Collections.frequency(listX, s), Collections.frequency(listY, s));
+                sb.append(s.repeat(count));
+            }
+        }
+
+        String result = sb.toString();
+        if (result.length() == 0) return "-1";
+        if (result.replace("0", "").length() == 0) return "0";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = new Solution131128().solution("100", "203045");
+        System.out.println(s);
+    }
+}
