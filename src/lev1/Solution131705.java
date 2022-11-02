994c9e8b2fa66d8cc45b7f302334db417477074e
gaon-park <do.judo1224@gmail.com>
2022-11-02 19:49:45 +0900

    삼총사

---
+++ src/lev1/Solution131705.java
@@ -0,0 +1,21 @@
+package lev1;
+
+public class Solution131705 {
+    public int solution(int[] number) {
+        int count = 0;
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    count = (number[i] + number[j] + number[k] == 0) ? count + 1 : count;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int res = new Solution131705().solution(new int[] {-1, 1, -1, 1});
+        System.out.println(res);
+    }
+}
